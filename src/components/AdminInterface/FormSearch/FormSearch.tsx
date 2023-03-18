ed375ebf5ed559fa8f1cceb2f2f76d1ea35074de
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-03-18 17:08:54 +0300

    init: test-searching

--- src/components/AdminInterface/FormSearch/FormSearch.tsx
+++ src/components/AdminInterface/FormSearch/FormSearch.tsx
@@ -3,21 +3,17 @@ import "./index.scss";
 import { PATHDOMAIN } from "../../../constants";
 
 const FormSearch = () => {
-  const [category, setCategory] = useState("");
   const [model, setModel] = useState("");
-  const [year, setYear] = useState("");
   const [search, setSearch] = useState("");
 
   const handlerSubmit = async (e) => {
     e.preventDefault();
 
     const req = await fetch(
-      `${PATHDOMAIN}admin/search?search=${search}&category=${category}&model=${model}&year=${year}`
+      `${PATHDOMAIN}admin/search?search=${search}&model=${model}&page=1&limit=5`
     );
-    setSearch("");
-    setYear("");
-    setModel("");
-    setCategory("");
+    const { results } = await req.json();
+    console.log(results);
   };
 
   return (
@@ -33,17 +29,6 @@ const FormSearch = () => {
       </div>
       <div className="form-search__options">
         <select
-          name="category"
-          id="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="блок управения">Блок управления</option>
-          <option value="крыша">Крыша</option>
-          <option value="Крыло">Крыло</option>
-        </select>
-
-        <select
           name="model"
           id="model"
           value={model}
@@ -53,17 +38,6 @@ const FormSearch = () => {
           <option value="Audi">Audi</option>
           <option value="Iveco">Iveco</option>
           <option value="x6">x6</option>
-        </select>
-
-        <select
-          name="year"
-          id="year"
-          value={year}
-          onChange={(e) => setYear(e.target.value)}
-        >
-          <option value="1999">1999</option>
-          <option value="2000">2000</option>
-          <option value="2001">2001</option>
         </select>
       </div>
     </form>
